4cce980c097bad47b1c21b45770ab7ae7b1b08da
samussamu <samutt90@gmail.com>
2021-12-10 15:08:49 +0100

    Login creado
    Login creado

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>@yield('title') - Laravel App</title>
+    
+    <!-- Tailwind CSS Link -->
+    <link rel="stylesheet" 
+    href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.1/tailwind.min.css">
+  </head>
+  <body class="bg-gray-100 text-gray-800 ">
+    <!-- Document body -->
+    <nav class="flex py-5 bg-indigo-500 text-white">
+        <div class="w-1/2 px-12 mr-auto">
+            <a href="{{redirect()->to('/')}}" class="text-2x1 font-bold"> Gym Laravel</a>
+            
+        </div>
+
+        <ul class="flex w-1/2 px-16 ml-auto justify-end pt-1">
+            @if (auth()->check())
+               <li class="mx-8">
+                  <p class="text-xl">Welcome <b>{{auth()->user()->name}}</b></p>
+               </li>
+               <li>
+                <a href="{{route('login.destroy')}}" class="font-bold  py-3 px-4 rounded-md bg-red-500 hover:bg-red-600 hover:text-indigo-700">Log Out</a>
+                </li>
+            @else
+            <li class="mx-6">
+                <a href="{{route('login.index')}}" class="font-semibold hover:bg-indigo-700 py-3 px-4 rounded-md mx-6"> Log In</a>
+            </li>
+            <li>
+             <a href="{{route('register.index')}}" class="font-semibold border 2 border-white py-2 px-4 rounded-md hover:bg-white hover:text-indigo-700"> Register</a>
+             </li>
+            @endif
+        </ul>
+
+    </nav>
+
+
+    @yield('content')
+
+
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\SessionsController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +15,26 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->middleware('auth');
+
+Route::get('/register',[RegisterController::class,'create'])
+->middleware('guest')
+->name('register.index');
+
+Route::post('/register',[RegisterController::class,'store'])
+->name('register.store');
+
+
+
+Route::get('/login',[SessionsController::class,'create'])
+->middleware('guest')
+->name('login.index');
+
+
+Route::post('/login',[SessionsController::class,'store'])
+->name('login.store');
+
+Route::get('/logout',[SessionsController::class,'destroy'])
+->middleware('auth')
+->name('login.destroy');
